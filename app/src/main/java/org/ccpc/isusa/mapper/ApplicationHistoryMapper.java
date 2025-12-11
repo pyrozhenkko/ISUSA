da44645bda62089cc71a7382c2d02086e5be3b27
pyrozhenkko <pyrozhenkko@gmail.com>
2025-12-11 03:02:03 +0200

    Add new mapper classes

--- app/src/main/java/org/ccpc/isusa/mapper/ApplicationHistoryMapper.java
+++ app/src/main/java/org/ccpc/isusa/mapper/ApplicationHistoryMapper.java
@@ -1,4 +1,23 @@
 package org.ccpc.isusa.mapper;
 
-public class ApplicationHistoryMapper {
+import org.ccpc.isusa.dto.response.ApplicationHistoryResponseDto;
+import org.ccpc.isusa.entity.ApplicationHistory;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ApplicationHistoryMapper {
+
+    @Mapping(source = "application.applicationId", target = "applicationId")
+    @Mapping(source = "application.title", target = "applicationTitle")
+    @Mapping(source = "status.statusId", target = "statusId")
+    @Mapping(source = "status.statusName", target = "statusName")
+    @Mapping(source = "changedByUser.userId", target = "changedByUserId")
+    @Mapping(source = "changedByUser.fullName", target = "changedByUserName")
+    @Mapping(source = "changedByUser.role.roleName", target = "changedByRole")
+    ApplicationHistoryResponseDto toDto(ApplicationHistory entity);
+
+    List<ApplicationHistoryResponseDto> toDtoList(List<ApplicationHistory> entities);
 }
--- app/src/main/java/org/ccpc/isusa/mapper/ApplicationReviewerMapper.java
+++ app/src/main/java/org/ccpc/isusa/mapper/ApplicationReviewerMapper.java
@@ -1,4 +1,29 @@
 package org.ccpc.isusa.mapper;
 
-public class ApplicationReviewerMapper {
+import org.ccpc.isusa.dto.request.ApplicationReviewerRequestDto;
+import org.ccpc.isusa.dto.response.ApplicationReviewerResponseDto;
+import org.ccpc.isusa.entity.ApplicationReviewer;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.Set;
+
+@Mapper(componentModel = "spring")
+public interface ApplicationReviewerMapper {
+
+    // --- Entity to Response DTO ---
+    @Mapping(source = "application.applicationId", target = "applicationId")
+    @Mapping(source = "reviewerUser.userId", target = "reviewerUserId")
+    @Mapping(source = "reviewerUser.username", target = "reviewerUserName")
+    ApplicationReviewerResponseDto toDto(ApplicationReviewer entity);
+
+    Set<ApplicationReviewerResponseDto> toDtoSet(Set<ApplicationReviewer> entities);
+
+    // --- Request DTO to Entity ---
+    // Ми ігноруємо поля, які встановимо в сервісі (application, reviewerUser, reviewedDate)
+    @Mapping(target = "application", ignore = true)
+    @Mapping(target = "reviewerUser", ignore = true)
+    @Mapping(target = "reviewedDate", ignore = true)
+    @Mapping(source = "isApprovedByTeacher", target = "isApproved") // Мапимо наше перейменоване поле
+    ApplicationReviewer toEntity(ApplicationReviewerRequestDto dto);
 }
