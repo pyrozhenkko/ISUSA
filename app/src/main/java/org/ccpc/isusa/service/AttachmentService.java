2fe368d04a856ed99d1074ef9a3cd4c9855c7f6d
pyrozhenkko <pyrozhenkko@gmail.com>
2026-01-02 21:58:04 +0200

    add allowed types for student files pt.2

--- app/src/main/java/org/ccpc/isusa/service/AttachmentService.java
+++ app/src/main/java/org/ccpc/isusa/service/AttachmentService.java
@@ -26,7 +26,9 @@ import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Slf4j
